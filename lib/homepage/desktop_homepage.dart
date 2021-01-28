a20ef914dd74e339caede6eaeaaeaaf434c8d119
helewrer3 <helewrer3@gmail.com>
2021-01-28 22:55:06 +0530

    Added AboutUs

--- lib/homepage/desktop_homepage.dart
+++ lib/homepage/desktop_homepage.dart
@@ -6,6 +6,7 @@ import 'package:vihaan_new/screensDesktop/team.dart';
 import 'package:vihaan_new/screensDesktop/timelinePage.dart';
 import 'package:vihaan_new/widgets/desktopNavbar.dart';
 import 'package:vihaan_new/widgets/vihaan_icons_icons.dart';
+import 'package:vihaan_new/screensDesktop/aboutus.dart';
 
 class DesktopHomePage extends StatefulWidget {
   DesktopHomePage({Key key}) : super(key: key);
@@ -71,7 +72,7 @@ class _DesktopHomePageState extends State<DesktopHomePage> {
                     child: Container(
                       height: MediaQuery.of(context).size.height,
                       width: MediaQuery.of(context).size.width,
-                      child: Stack(children: []),
+                      child: Stack(children: [AboutUs()]),
                     ),
                   ),
                   _wrapScrollTag(
