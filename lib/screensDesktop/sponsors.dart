30e915f95b5c699bb01286c808aa8da408433774
helewrer3 <helewrer3@gmail.com>
2021-02-03 10:42:06 +0530

    debugging

--- lib/screensDesktop/sponsors.dart
+++ lib/screensDesktop/sponsors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:vihaan_new/widgets/sponsorCard.dart';
 
 class Sponsors extends StatelessWidget {
-  final double mrg = 400, gold = 215, silver = 175, bronze = 150, title_fnt = 70;
+  final double mrg = 2, gold = 215, silver = 175, bronze = 150, title_fnt = 70;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,40 +30,37 @@ class Sponsors extends StatelessWidget {
             Container(height: 2, color: Colors.black12, margin: EdgeInsets.symmetric(horizontal: 350)),
             SponsorTitle(title: 'GOLD', fnt: title_fnt, clr: Colors.amber[500]),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SponsorCard(bx: gold, mrl: mrg, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: gold, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: gold, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: gold, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: gold, mrl: 0, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg')
+                SponsorCard(bx: gold, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: gold, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: gold, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: gold, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg')
               ],
             ),
             SizedBox(height: 15),
             SponsorTitle(title: 'SILVER', fnt: title_fnt, clr: Color.fromARGB(255, 192, 192, 192)),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SponsorCard(bx: silver, mrl: mrg, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: silver, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: silver, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: silver, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: silver, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: silver, mrl: 0, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg')
+                SponsorCard(bx: silver, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: silver, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: silver, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: silver, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: silver, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg')
               ],
             ),
             SizedBox(height: 15),
             SponsorTitle(title: 'BRONZE', fnt: title_fnt, clr: Color.fromARGB(255, 205, 127, 50)),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SponsorCard(bx: bronze, mrl: mrg, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: bronze, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: bronze, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: bronze, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: bronze, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: bronze, mrl: 0, mrr: 0, url: 'images/Vihaan_Aboutus.jpg'),
-                SponsorCard(bx: bronze, mrl: 0, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg')
+                SponsorCard(bx: bronze, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: bronze, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: bronze, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: bronze, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: bronze, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg'),
+                SponsorCard(bx: bronze, mrl: mrg, mrr: mrg, url: 'images/Vihaan_Aboutus.jpg')
               ],
             )
           ],
