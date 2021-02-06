3985c16f5576d794f22d0a65d39be60291f6cdef
Shubham6013 <75625415+Shubham6013@users.noreply.github.com>
2021-02-06 11:44:04 +0530

    Update mobile home page

--- lib/homepage/mobile_homepage.dart
+++ lib/homepage/mobile_homepage.dart
@@ -2,6 +2,10 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
+import 'package:vihaan_new/screensDesktop/aboutus.dart';
+import 'package:vihaan_new/screensmobile/landingpage_m.dart';
+import 'package:vihaan_new/screensDesktop/team.dart';
+import 'package:vihaan_new/screensDesktop/timelinePage.dart';
 
 class MobileHomePage extends StatefulWidget {
   @override
@@ -13,13 +17,20 @@ class _MobileHomePageState extends State<MobileHomePage> {
   final random = math.Random();
   final scrollDirection = Axis.vertical;
 
-  AutoScrollController controller;
+  AutoScrollController _controller;
   List<List<int>> randomList;
+  Widget _wrapScrollTag({int index, Widget child}) => AutoScrollTag(
+        key: ValueKey(index),
+        controller: _controller,
+        index: index,
+        child: child,
+        //highlightColor: Colors.black.withOpacity(0.1),
+      );
 
   @override
   void initState() {
     super.initState();
-    controller = AutoScrollController(axis: scrollDirection);
+    _controller = AutoScrollController(axis: scrollDirection);
     randomList = List.generate(maxCount,
         (index) => <int>[index, (1000 * random.nextDouble()).toInt()]);
   }
@@ -28,18 +39,19 @@ class _MobileHomePageState extends State<MobileHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         centerTitle: true,
-        title: Text('Vihaan Logo'),
+        title: Text('VIHAAN'),
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
-              child: Text('Vihaan Logo or Image'),
               decoration: BoxDecoration(
-                color: Colors.blue,
+                color: Colors.black,
               ),
+              child: Image(image: AssetImage("images/Vihaan_Logo.png")),
             ),
             ListTile(
               title: Text('About Us'),
@@ -49,56 +61,56 @@ class _MobileHomePageState extends State<MobileHomePage> {
               },
             ),
             ListTile(
-              title: Text('Item 1'),
+              title: Text('Tracks'),
               onTap: () {
                 // Update the state of the app.
                 // ...
               },
             ),
             ListTile(
-              title: Text('Item 1'),
+              title: Text('Prizes'),
               onTap: () {
                 // Update the state of the app.
                 // ...
               },
             ),
             ListTile(
-              title: Text('Item 1'),
+              title: Text('Timeline'),
               onTap: () {
                 // Update the state of the app.
                 // ...
               },
             ),
             ListTile(
-              title: Text('Item 1'),
+              title: Text('Judges'),
               onTap: () {
                 // Update the state of the app.
                 // ...
               },
             ),
             ListTile(
-              title: Text('Item 1'),
+              title: Text('Sponsors'),
               onTap: () {
                 // Update the state of the app.
                 // ...
               },
             ),
             ListTile(
-              title: Text('Item 1'),
+              title: Text('FAQs'),
               onTap: () {
                 // Update the state of the app.
                 // ...
               },
             ),
             ListTile(
-              title: Text('Item 1'),
+              title: Text('Team'),
               onTap: () {
                 // Update the state of the app.
                 // ...
               },
             ),
             ListTile(
-              title: Text('Item 2'),
+              title: Text('Contact Us'),
               onTap: () {
                 // Update the state of the app.
                 // ...
@@ -108,12 +120,108 @@ class _MobileHomePageState extends State<MobileHomePage> {
         ),
       ),
       body: SingleChildScrollView(
-        controller: controller,
+        controller: _controller,
         child: Column(
           children: [
             //TODO: Vihaan Home Page
-            Container(
-              child: Text('About Us Section'),
+            _wrapScrollTag(
+              index: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Stack(children: [
+                  FancyBackgroundApp(),
+                  LandingPageContent(),
+                ]),
+              ),
+            ),
+            _wrapScrollTag(
+              index: 1,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Stack(children: [
+                  AboutUs(),
+                ]),
+              ),
+            ),
+            _wrapScrollTag(
+              index: 2,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.green,
+              ),
+            ),
+            _wrapScrollTag(
+              index: 3,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.blue,
+              ),
+            ),
+            _wrapScrollTag(
+              index: 4,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 1.5,
+                width: MediaQuery.of(context).size.width,
+                child: Stack(children: [
+                  TimelinePage(),
+                ]),
+              ),
+            ),
+            _wrapScrollTag(
+              index: 5,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.blueAccent,
+                child: Stack(children: []),
+              ),
+            ),
+            _wrapScrollTag(
+              index: 6,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 1.15,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.green,
+                child:
+                    /*Stack(
+                        children: [Sponsors()],
+                      )*/
+                    Container(
+                  color: Colors.orange,
+                ),
+              ),
+            ),
+            _wrapScrollTag(
+              index: 7,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.blue,
+              ),
+            ),
+            _wrapScrollTag(
+              index: 8,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.greenAccent,
+                child: Stack(
+                  children: [
+                    TeamSection(),
+                  ],
+                ),
+              ),
+            ),
+            _wrapScrollTag(
+              index: 9,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.redAccent,
+              ),
             ),
           ],
         ),
