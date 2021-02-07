808eeab23ef4ea3a64f07b81cfe402718d896f51
Shubham6013 <75625415+Shubham6013@users.noreply.github.com>
2021-02-07 15:23:29 +0530

    updated mobile screens

---
+++ lib/screensMobile/aboutus_m.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class AboutUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var _width = MediaQuery.of(context).size.width;
+    var _height = MediaQuery.of(context).size.height;
+
+    return Container(
+      color: Color.fromARGB(255, 222, 240, 244),
+      child: Row(
+        children: [
+          Flexible(
+              child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: _width * 0.05),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(30, 20, 30, 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'ABOUT US',
+                      style: TextStyle(
+                          fontSize: 50,
+                          fontFamily: 'NunitoSans',
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    ),
+                    SizedBox(height: 20),
+                    Center(
+                      child: Text(
+                        'One of the biggest IEEE Student branches in Delhi, IEEE DTU has emerged as the most active & prolific student organisation in the past 35 years. With over 400 active student members. IEEE DTU provides students extensive opportunities for skill development in various domains of engineering by actively organising seminars, SIGs and workshops by senior members of the organisation and collaborating with other premier institutions. Vihaan is an annual event organized by IEEE DTU, a university wide professional organization dedicated to encourage students to take up and continue their careers in STEM Fields. IEEE DTU believes in encouraging talent that is not bounded by gender inequalities and with Vihaan, IEEE DTU aims at carrying this thought forward. The event is a 24 hour Hackathon which provides a platform to budding programmers to come up with a solution to an existing problem using technology. Students can participate in a team size of upto 4 members.',
+                        style: TextStyle(
+                            //fontSize: 20,
+                            fontFamily: 'NunitoSans',
+                            color: Colors.white),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          // height: _height * 0.60,
+                          // width: _width * 0.60,
+                          child: Image(
+                            fit: BoxFit.fill,
+                            image: AssetImage("images/Vihaan_Aboutus.jpg"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              elevation: 5.0,
+              color: Color.fromARGB(200, 79, 174, 196),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
+          )),
+
+          // Flexible(
+          //     child: Container(
+          //   padding: EdgeInsets.symmetric(horizontal: _width * 0.05),
+          //   child: Image(image: AssetImage('images/Vihaan_Aboutus.jpg')),
+          // ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screensMobile/contactus_m.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class ContactUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var contactUsTextStyle =
+        TextStyle(fontSize: 16, fontWeight: FontWeight.w200, height: 1.3);
+    return Container(
+      padding: EdgeInsets.only(top: 32, bottom: 16),
+      color: Colors.indigoAccent,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(
+                  constraints: BoxConstraints(maxHeight: 200),
+                  child: Image.asset(
+                    'images/IEEE_DTU_Logo.png',
+                    color: Colors.white70,
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(
+                  constraints: BoxConstraints(maxHeight: 200),
+                  child: Image.asset(
+                    'images/WIE_Logo_Black.png',
+                    color: Colors.white70,
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 3,
+                child: Column(
+                  children: [
+                    Container(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: RichText(
+                              text: TextSpan(
+                                text: 'Ritwik ',
+                                children: [
+                                  TextSpan(
+                                    text: 'Ranjan',
+                                    style: TextStyle(
+                                        backgroundColor: Colors.white70,
+                                        color: Colors.black87,
+                                        height: 1.8),
+                                  ),
+                                  TextSpan(
+                                    text: '\nLead Organizer, VIHAAN',
+                                    style: contactUsTextStyle,
+                                  ),
+                                  TextSpan(
+                                    text:
+                                        '\nMembership & Webinar Coordinator, IEEE DTU',
+                                    style: contactUsTextStyle,
+                                  ),
+                                  TextSpan(
+                                    text: '\n+91 98733 88660',
+                                    style: contactUsTextStyle,
+                                  ),
+                                  TextSpan(
+                                    text: '\nritwikr@ieee.org',
+                                    style: contactUsTextStyle,
+                                  ),
+                                ],
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontFamily: 'NunitoSans',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                  height: 1.2,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Center(
+            child: Text(
+              'Copyright: IEEE DTU',
+              style: TextStyle(fontSize: 16, color: Colors.white70),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
