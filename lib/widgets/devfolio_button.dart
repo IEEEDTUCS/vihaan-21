97171d6d316863c3fb6ce24ac5e2a38399a05e21
ritwikranjan <ritwikranjan99@gmail.com>
2021-03-04 22:43:03 +0530

    Devfolio

---
+++ lib/widgets/devfolio_button.dart
@@ -0,0 +1,40 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart';
+
+class DevfolioButton extends StatefulWidget {
+  @override
+  _DevfolioButtonState createState() => _DevfolioButtonState();
+}
+
+class _DevfolioButtonState extends State<DevfolioButton> {
+  @override
+  void initState() {
+    // ignore: undefined_prefixed_name
+    final _divElement = IFrameElement();
+    _divElement.src = 'https://devfoliovihaan.web.app/';
+    _divElement.style.border = 'none';
+    _divElement.style.padding = '0';
+    _divElement.style.margin = '0';
+
+    /*_divElement.attributes = {
+      //'scrolling': 'no',
+    };*/
+    ui.platformViewRegistry
+        .registerViewFactory('divElement', (int viewID) => _divElement);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 22),
+      color: Colors.transparent,
+      height: 65,
+      width: 320,
+      child: HtmlElementView(viewType: 'divElement'),
+    );
+  }
+}
