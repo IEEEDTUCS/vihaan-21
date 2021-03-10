103a07611f294449d74bbf765ff520624d884436
ritwikranjan <ritwikranjan99@gmail.com>
2021-03-10 17:12:30 +0530

    Deleted Android and Ios

--- android/app/src/main/kotlin/com/ieeedtu/vihaan2021/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ieeedtu.vihaan2021
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/ieeedtu/vihaan_new/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ieeedtu.vihaan_new
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
